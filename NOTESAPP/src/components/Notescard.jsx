80f055bae54a5189ee0d75919570fff379122bc9
KowshikP07 <kowshikp.23cse@kongu.edu>
2025-11-28 21:35:25 +0530

    notes container has been developed

--- NOTESAPP/src/components/Notescard.jsx
+++ NOTESAPP/src/components/Notescard.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Notescard = () => {
+    const [activecont,setactive]=useState(null)
+    const handleclick=(id)=>{
+        setactive(id===activecont?null:id)
+    }
   return (
       <div className="content">
-        <div className="card">
+        <div className={`card ${activecont===1 ? 'active' : ''}`}>
             <div className="cardtop"></div>
-            <div className="cardbottom"></div>
+            <div className="notescontent"></div>
+            <div className="notescontent"></div>
+            <div className="notescontent"></div>
+            <div className="notescontent"></div>
+            <div className="cardbottom"><button className='cardbtn' onClick={()=>handleclick(1)}>Tap to unlock</button></div>
         </div>
-        <div className="card">
+        <div className={`card ${activecont===2 ? 'active' : ''}`}>
             <div className="cardtop"></div>
-            <div className="cardbottom"></div>
+            <div className="notescontent"></div>
+            <div className="notescontent"></div>
+            <div className="notescontent"></div>
+            <div className="notescontent"></div>
+            <div className="cardbottom"><button className='cardbtn' onClick={()=>handleclick(2)}>Tap to unlock</button></div>
         </div>
-        <div className="card">
+        <div className={`card ${activecont===3 ? 'active' : ''}`}>
             <div className="cardtop"></div>
-            <div className="cardbottom">
-                <button className='cardbtn'></button>
-            </div>
+            <div className="notescontent"></div>
+            <div className="notescontent"></div>
+            <div className="notescontent"></div>
+            <div className="notescontent"></div>
+            <div className="cardbottom"><button className='cardbtn' onClick={()=>handleclick(3)}>Tap to unlock</button></div>
         </div>
       </div>
   )
