0c5e770fd7fe6e3008fd76635b251c27317f431a
KowshikP07 <kowshikp.23cse@kongu.edu>
2025-11-30 23:58:52 +0530

    input box for adding notes is created

--- NOTESAPP/src/components/Note.jsx
+++ NOTESAPP/src/components/Note.jsx
@@ -3,7 +3,10 @@ import React, { useState } from 'react'
 const Note = () => {
   return (
     <div className='creatingfiles'>
-      notes
+      <div className="notesmain">
+        <div className="notetop"><div className="notescir"></div><input className="notetitle" type='text'/></div>
+        <div className="notestcontentuser"></div>
+      </div>
     </div>
   )
 }
