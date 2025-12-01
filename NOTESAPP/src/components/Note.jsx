673cdd1c525451f4e2e1077614338f2b5636630a
KowshikP07 <kowshikp.23cse@kongu.edu>
2025-12-01 23:08:05 +0530

    trying to add the input feature for notesapp

--- NOTESAPP/src/components/Note.jsx
+++ NOTESAPP/src/components/Note.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
-
 const Note = () => {
+  const [usercontent,setcontent]=useState({"title":"","text":""})
+  function handlechange(e){
+    // const {}
+  }
   return (
     <div className='creatingfiles'>
       <div className="notesmain">
-        <div className="notetop"><div className="notescir"></div><input className="notetitle" type='text'/></div>
-        <div className="notestcontentuser"></div>
+        <div className="notetop"><div className="notescir"></div><input value={usercontent.title} className="notetitle" type='text' placeholder='Enter the Category' onChange={handlechange}/></div>
+        <div className="notestcontentuser"><textarea className='textarea' placeholder='Enter the notes...'/></div>
       </div>
     </div>
   )
