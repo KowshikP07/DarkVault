4329b279ef423a3b6027dd414c7ce85e62008ba7
KowshikP07 <kowshikp.23cse@kongu.edu>
2025-12-02 22:37:18 +0530

    trying to add the category feature

--- NOTESAPP/src/components/Folder.jsx
+++ NOTESAPP/src/components/Folder.jsx
@@ -2,9 +2,12 @@ import React, { useState } from 'react'
 
 const Folder = () => {
   const [folder,setfolder]=useState(null);
+  function handlechange(){
+    console.log("handling changes")
+  }
   return (
     <div className='creatingfolder'>
-      folder
+      <div className="notetop"><div className="notescir"></div><input name="title" className="notetitle" type='text' placeholder='Enter the Category' onChange={handlechange}/></div>
     </div>
   )
 }
--- NOTESAPP/src/components/Note.jsx
+++ NOTESAPP/src/components/Note.jsx
@@ -1,14 +1,25 @@
 import React, { useState } from 'react'
 const Note = () => {
   const [usercontent,setcontent]=useState({"title":"","text":""})
+
   function handlechange(e){
     // const {}
+    const {name,value}=e.target;
+    setcontent(prev=>{
+      return {...prev,[name]:value};
+    })
+  }
+  function handlesubmit(e){
+    e.preventDefault();
+    console.log(usercontent.title+" "+usercontent.text);
+    setcontent({title:"",text:""});
   }
   return (
     <div className='creatingfiles'>
-      <div className="notesmain">
-        <div className="notetop"><div className="notescir"></div><input value={usercontent.title} className="notetitle" type='text' placeholder='Enter the Category' onChange={handlechange}/></div>
-        <div className="notestcontentuser"><textarea className='textarea' placeholder='Enter the notes...'/></div>
+      <div className="notesmain"><form onSubmit={handlesubmit}>
+        <div className="notetop"><div className="notescir"></div><input name="title" value={usercontent.title} className="notetitle" type='text' placeholder='Enter the Category' onChange={handlechange}/></div>
+        <div className="notestcontentuser"><textarea name="text" value={usercontent.text} type="text" className='textarea' placeholder='Enter the notes...' onChange={handlechange}/></div>
+        <button type="submit" className='notesubmit'>Save!</button></form>
       </div>
     </div>
   )
