822593da5b8229ee4b704d4cb45fdf770ed792db
KowshikP07 <kowshikp.23cse@kongu.edu>
2025-12-02 23:41:59 +0530

    yet to add the feature that work with the input tags

--- NOTESAPP/src/components/Folder.jsx
+++ NOTESAPP/src/components/Folder.jsx
@@ -1,13 +1,25 @@
 import React, { useState } from 'react'
 
 const Folder = () => {
-  const [folder,setfolder]=useState(null);
-  function handlechange(){
+  const [folder,setfolder]=useState({"category":""});
+  function handlechange(e){
+    const {name,value}=e.target;
+    setfolder(prev=>({
+       ...prev,[name]:value
+    }));
     console.log("handling changes")
+  }
+  function handlesubmit(e){
+    e.preventDefault();
+    console.log("form submited")
+    setfolder(prev=>({...prev,category:""}))
   }
   return (
     <div className='creatingfolder'>
-      <div className="notetop"><div className="notescir"></div><input name="title" className="notetitle" type='text' placeholder='Enter the Category' onChange={handlechange}/></div>
+      <form onSubmit={handlesubmit} className='folderform'>
+      <div className="notetop1"><div className="notescir"></div><input name="category" className="notetitle" value={folder.category} type='text' placeholder='Enter the Category' onChange={handlechange}/></div>
+      <button type='submit' className='notesubmit'>Save!</button>
+      </form>
     </div>
   )
 }
